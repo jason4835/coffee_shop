fab19c643559f24f71b48b0a52aaed20cae48675
Jason Paz <81496450+jason4835@users.noreply.github.com>
2021-12-22 10:36:54 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# cofee shop ask customer what they want small, medium, large two options dark roast and light roast, add prices with final total saying "Here is you [roast_type], [size] sized coffee would you like milk" ask if customer wants milk then give final toatal
+
+# NOTE: WHEN USING YOU MAY NEED TO MAKE THE CONSOLE LARGER
+
+from functions import welcome, roast
+
+welcome()
+roast()
